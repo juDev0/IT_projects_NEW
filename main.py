72b2fb94d9be39719e8daa4f9485d309e5761ce7
joshua <udiajoshua@gmail.com>
2025-08-05 07:38:14 -0800

    added the edit feature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 """Tracker Application."""
+
 import sys
 from PyQt5.QtWidgets import(
     QApplication, QWidget, QVBoxLayout, 
@@ -19,15 +20,19 @@ class AssignmentTracker(QWidget):
     def __init__(self):
         super().__init__()
 
+       
         self.apply_styles()
         self.setWindowTitle("Assignment Deadline Tracker")
         self.setGeometry(400,200,500,500)
         self.init_ui()
-        self.assignmets = []  # this holds all the assingmetn reason: so it can be sorted
+        self.assignments = []  # this holds all the assingmetn reason: so it can be sorted
         self.load_assignments()  # this automatically loads data from the file whan lunched
+        self.show_due_reminders()
 
     def init_ui(self):
         layout = QVBoxLayout()
+
+        special_layout = QHBoxLayout()
 
         # The Title input
         self.title_input = QLineEdit()
@@ -43,6 +48,23 @@ class AssignmentTracker(QWidget):
         self.add_button = QPushButton("Add Assignemt")
         self.add_button.clicked.connect(self.add_assignment)
 
+        # edit Button
+        self.edit_button = QPushButton("Edit")
+        self.edit_button.clicked.connect(self.edit_assignment)
+        self.edit_button.setStyleSheet("""
+            QPushButton {
+                background-color: green;
+                color: white;
+                border: none;
+                border-radius: 6px;
+                padding: 6px 12px;
+                font-size: 17px;
+                font-weight: bold;
+            }
+            QPushButton:hover {
+                background-color: darkGreen;
+            }
+        """)
 
         # exit Button
         self.exit_button = QPushButton("Exit")
@@ -73,9 +95,9 @@ class AssignmentTracker(QWidget):
         # Delete Button
         self.delete_button = QPushButton("Delete")
         self.delete_button.clicked.connect(self.delete_assignment)
+        new_layout.addWidget(self.edit_button)
         new_layout.addWidget(self.delete_button)
         new_layout.addWidget(self.exit_button)
-
 
         layout.addLayout(input_layout)
         layout.addWidget(QLabel("Upcoming assignments:"))
@@ -143,32 +165,32 @@ class AssignmentTracker(QWidget):
 
         if title.strip():
             # Add the assignment to our internal list
-            self.assignmets.append({
+            self.assignments.append({
                 "title": title,
                 "due": due_date
             })
 
             # Sort the list by due date
-            self.assignmets.sort(key=lambda x: x["due"])
+            self.assignments.sort(key=lambda x: x["due"])
 
             # Clear the QListWidget before updating
             self.assignment_list.clear()
 
             # Add sorted .items to the  list
             today = QDate.currentDate()
-            for item in self.assignmets:
+            for item in self.assignments:
                 due_str = item["due"].toString("yyyy-MM-dd")
                 days_left = today.daysTo(item["due"])
                 text = f"{item['title']} - Due: {due_str}........({days_left} days left)"
                 if days_left < 0:
                     text += " [OVERDUE]"
                     item = QListWidgetItem(text)
-                    item.setForeground(Qt.red)
+                    item.setForeground(Qt.red) 
 
                 elif days_left == 0:
                     text += " [DUE]"
                     item = QListWidgetItem(text)
-                    item.setForeground(Qt.darkYellow)
+                    item.setForeground(Qt.darkBlue)
                     
                 else:
                     item =QListWidgetItem(text)
@@ -185,27 +207,27 @@ class AssignmentTracker(QWidget):
     # saves
     def save_assignments(self):
         data = []
-        for item in self.assignmets:
+        for item in self.assignments:
             data.append({
                 "title": item["title"],
                 "due": item["due"].toString("yyyy-MM-dd")
             })
 
-        with open("assignmets.json", "w") as file:
+        with open("assignments.json", "w") as file:
             json.dump(data, file, indent=2)
 
 
     # loads
     def load_assignments(self):
-        if os.path.exists("assignmets.json"):
-            with open("assignmets.json", "r") as file:
+        if os.path.exists("assignments.json"):
+            with open("assignments.json", "r") as file:
                 data = json.load(file)
 
-            self.assignmets.clear()
+            self.assignments.clear()
 
             for item in data:
                 due_date = QDate.fromString(item["due"], "yyyy-MM-dd")
-                self.assignmets.append({
+                self.assignments.append({
                     "title": item["title"],
                     "due": due_date
                 })
@@ -220,7 +242,7 @@ class AssignmentTracker(QWidget):
         self.assignment_list.clear()
         today = QDate.currentDate()
 
-        for task in self.assignmets:
+        for task in self.assignments:
             title = task["title"]
 
             due_date = task["due"]
@@ -250,14 +272,14 @@ class AssignmentTracker(QWidget):
     def delete_assignment(self):
         selected_row = self.assignment_list.currentRow()
 
-        if selected_row >= 0:
+        if selected_row >= 0:  # this happens when a row is picked from the QlistWidget
             reply = QMessageBox.information(self,
             "Confirm Delete",
             "Are you sure you want to delete this?",
             QMessageBox.Yes|QMessageBox.No)
 
             if reply == QMessageBox.Yes:
-                del self.assignmets[selected_row]  # delets the item in the self.ass..list
+                del self.assignments[selected_row]  # delets the item in the self.ass..list
                 self.refresh_assignment_list()  # this will be added from the data available
                 self.save_assignments()  # this helps to save it to the file
         else:
@@ -274,11 +296,53 @@ class AssignmentTracker(QWidget):
             if reply == QMessageBox.Yes:
                 sys.exit()        
 
+    # this reminder is manual as user opens the application
+    def show_due_reminders(self):
+        today = QDate.currentDate()
+        due_today = []
+        overdue = []
+
+        for task in self.assignments:
+            days_left = today.daysTo(task["due"])
+
+            if days_left < 0:
+                overdue.append(task["title"])
+            elif days_left == 0:
+                due_today.append(task["title"])
+
+        message = ""
+        if overdue:
+            message += "Due Today:\n" + "\n".join(f"• {t}" for t in overdue)
+
+        if due_today:
+            message += "Due Today:\n" + "\n".join(f"• {t}" for t in due_today)
+
+
+        if message:
+            QMessageBox.information(self,"Assignment Reminder", message)
+
+
+    
+    def edit_assignment(self):
+        selected_row = self.assignment_list.currentRow()
+        if selected_row < 0:
+            QMessageBox.warning(self,"Warnning","Please selet an assignment to edit ")
+            return
+
+        # Loads selected assignmet details
+        # just like reversing them, gettin salt out of a water
+        task = self.assignments[selected_row]
+        self.title_input.setText(task["title"])
+        self.date_input.setDate(task["due"])
+
+        # Remove the old task temporarly so it can be re-added
+        del self.assignments[selected_row]
+        self.assignment_list.takeItem(selected_row)
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = AssignmentTracker()
     window.show()
     sys.exit(app.exec())
-
-    QMessageBox.warn
